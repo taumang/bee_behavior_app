7155d5b1113cc6e99a0ab8e8f1c3c2f3585b6780
Your Name <09taumang@gmail.com>
2023-05-07 14:49:24 +0200

    Added a bit of styling in the frontend. Looking to do more

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,16 +13,18 @@ const App = () => {
   },[])
 
   return (
-    <div>
-      <h1>Bee Behaviors</h1>
-      <ul>
-        {behaviors.map(behavior => (
-          <li key={behavior.id}>
-            <h2>{behavior.name}</h2>
-            <p>{behavior.description}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="bg-black min-h-screen">
+      <div className="bg-yellow-400 p-4">
+        <h1 className="text-3xl font-bold text-white mb-4">Bee Behaviors</h1>
+        <ul>
+          {behaviors.map((behavior) => (
+            <li key={behavior.id} className="mb-4">
+              <h2 className="text-xl font-bold text-white">{behavior.name}</h2>
+              <p className="text-gray-200">{behavior.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
