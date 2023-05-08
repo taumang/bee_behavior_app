1f2ea71524c8bfdf7faf7ea5bbdcee1b0e58b9e9
Your Name <09taumang@gmail.com>
2023-05-08 08:00:24 +0200

    Updated the data model for the bee behaviours. more updates are coming

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,15 @@ const app = express()
 const behaviors = [
   { id: 1, name: 'Pollination', description: 'Gathering nectar and pollen to pollinate flowers.' },
   { id: 2, name: 'Honey Production', description: 'Collecting nectar to make honey.' },
-  { id: 3, name: 'Nest Building', description: 'Building nests to lay eggs and raise young.' }
+  { id: 3, name: 'Nest Building', description: 'Building nests to lay eggs and raise young.' },
+  { id: 4, name: 'Wax Production', description: 'Producing beeswax for construction and storage.' },
+  { id: 5, name: 'Guarding', description: 'Protecting the hive from intruders.' },
+  { id: 6, name: 'Foraging', description: 'Searching for food sources outside the hive.' },
+  { id: 7, name: 'Temperature Regulation', description: 'Maintaining optimal temperature within the hive.' },
+  { id: 8, name: 'Communication', description: 'Using dances and pheromones to communicate with other bees.' },
+  { id: 9, name: 'Reproduction', description: 'Mating and ensuring the survival of the colony.' }
 ];
+
 
 //activating cors (to link with the frontend)
 app.use(cors())
